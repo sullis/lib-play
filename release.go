9c04cbf48c67d957851527572860408b50ddb207
Eric Mittelhammer <em@flow.io>
2017-10-04 16:07:11 -0400

    don't release this with a script since it's experimental

--- release.go
+++ release.go
@@ -1,12 +1,15 @@
 package main
 
-import (
+/*import (
 	"github.com/flowcommerce/tools/executor"
-)
+)*/
 
 func main() {
-	executor := executor.Create("lib-play")
-	executor = executor.Add("dev tag")
+  println("This is an experimental branch. Do this instead: https://www.notion.so/flow/Publish-an-experimental-library-f9de8cfb7a904af4b873641937601ca0")
+        /*
+        executor := executor.Create("lib-play-play-2.6")
+        executor = executor.Add("dev tag")
 	executor = executor.Add("sbt +publish")
 	executor.Run()
+        */
 }
